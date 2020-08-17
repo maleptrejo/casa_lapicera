78c4a4c46e8b98ee8cce95a5ad842709b8ad1faf
Malena Trejo <malenatrejo@MacBook-Pro-de-Malena.local>
2020-08-17 11:16:41 -0300

    modelos

--- lapiceras_express/database/models/Refill.js
+++ lapiceras_express/database/models/Refill.js
@@ -40,35 +40,10 @@ module.exports = (sequelize, dataTypes) => {
     const Refill = sequelize.define(alias, cols, config)
 
     Refill.associate = (models) => {
-    //    Product.belongsTo(models.Brands, {
-    //        as: "brands",
-    //        foreignKey: "brand_id"
-    //    })
-    //    Product.belongsTo(models.Discounts, {
-    //         as: "discounts",
-    //         foreignKey: "discount_id"
-    //     })
-        // Product.belongsTo(models.Categories, {
-        //     as: "categories",
-        //     foreignKey: "category_id"
-        // })
-        // Product.hasMany(models.Product_imgs, {
-        //     as:"product_imgs",
-        //     foreignKey: "product_id"
-        // })
-        // Product.belongsToMany(models.Colors,{
-        //     as: 'colors',
-        //     through: 'products_colors',
-        //     foreignKey: 'product_id',
-        //     otherKey:'color_id'
-        // })
-        //  Product.belongsToMany(models.Ocasions,{
-        //     as: 'ocasions',
-        //     through: 'products_ocasions',
-        //     foreignKey: 'product_id',
-        //     otherKey:'color_id'
-        // })
-     
+       Refill.belongsTo(models.Supplies, {
+           as: "refills",
+           foreignKey: "supply_id"
+       })     
     }
 
     return Refill;
